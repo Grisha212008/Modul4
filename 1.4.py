4a2dd60e38fe886c4726e51c4a6b7c1f737bfaf6
Grisha212008 <139346306+Grisha212008@users.noreply.github.com>
2023-08-07 22:33:05 +0300

    commit1
    commit

---
+++ 1.4.py
@@ -0,0 +1,9 @@
+def strconter(s):
+    syms_counter ={}
+    for sym in s:
+        syms_counter[sym] = syms_counter. get (sym, 0) + 1
+    for sym, count in syms_counter.items():
+        print(sym,count)
+        
+            
+strconter('aabc')
